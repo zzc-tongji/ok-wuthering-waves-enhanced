8303089e5090c1d449c91cfd45ad4fab7d1eb006
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-07-06 08:48:29 +0800

    patch

--- src/task/DailyTask2.py
+++ src/task/DailyTask2.py
@@ -51,7 +51,7 @@ class DailyTask2(WWOneTimeTask, BaseCombatTask):
             'Exit with Error': True,
         }
         self.config_description = {
-            'Which Weekly Boss to Teleport': 'For Example, Denia, From Top to Bottom, Starting with 1',
+            'Which Weekly Boss to Teleport': 'From Top to Bottom, Starting with 1',
             'Boss Level': "Choose the Lowest that Drop a Echo",
             'Boss': 'Select boss profile (includes Combat Wait Time)',
             'Combat Wait Time': 'Wait time before each combat (seconds), overrides Boss profile if set',
