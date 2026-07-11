6e3e8b094477b9e5bbf9c81a600c1c5fd447dd44
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2026-07-12 00:37:46 +0800

    sync

--- src/task/EnhancedUtils.py
+++ src/task/EnhancedUtils.py
@@ -5,7 +5,7 @@ logger = Logger.get_logger(__name__)
 
 def hijack_use_stamina(task_class):
     #
-    def use_stamina(self, once, must_use=0):
+    def use_stamina(self, once=60, must_use=0): 
         self.sleep(1)
         current, back_up, total = self.get_stamina()
         y = 0.62
